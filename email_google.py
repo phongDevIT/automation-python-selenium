4b8d58f6ba218f40bf41b46d07647a1f7cb3b688
phongDevIT <nemo24112003357@gmail.com>
2023-09-06 11:09:39 +0700

    push

--- email_google.py
+++ email_google.py
@@ -1,25 +1,29 @@
 from seleniumbase import SB
 import time
-from selenium.webdriver.common.keys import Keys
 
-def access_google_ads(sb):
-    email = {
-        "email": "nguyendiphong159@gmail.com",
-        "pass_login": "nguyenngocphong3121999",
-    }
-    sb.open("https://ads.google.com/")
-    sb.click('//*[@id="header-topbar"]/div/div[3]/div/div[4]/a', timeout=5)
-    sb.type('input[type="email"]', email["email"], timeout=5)
-    sb.click('button:contains("Next")', timeout=15, delay=1)
-    sb.type('input[type="password"]', email["pass_login"], timeout=5)
-    sb.click('button:contains("Next")', timeout=20, delay=1)
-    sb.wait_for_element_absent('button:contains("Next")', timeout=5)
-    print("successfully access google ads")
-    time.sleep(20)
-    sb.click('//*[@id="navigation.tools"]/div/a/rail-item', timeout=15, delay=1)
-    sb.click(
-        "/html/body/div[1]/root/div/div[1]/div[2]/div/div[3]/div/div/awsm-child-content/div[1]/div[2]/kp-root/div/div/view-loader[2]/splash-view/div/div/div[1]/splash-cards/div/div[1]",
-        timeout=15,
-        delay=1,
-    )
-    print("click success 2")
+class EmailGoogleAccess:
+    def __init__(self, sb):
+        self.sb = sb
+
+    def access_google_ads(self):
+        email = {
+            "email": "nguyendiphong159@gmail.com",
+            "pass_login": "nguyenngocphong3121999",
+        }
+        self.sb.open("https://ads.google.com/")
+        self.sb.click('//*[@id="header-topbar"]/div/div[3]/div/div[4]/a', timeout=5)
+        self.sb.type('input[type="email"]', email["email"], timeout=5)
+        self.sb.click('button:contains("Next")', timeout=15, delay=1)
+        self.sb.type('input[type="password"]', email["pass_login"], timeout=5)
+        self.sb.click('button:contains("Next")', timeout=20, delay=1)
+        self.sb.wait_for_element_absent('button:contains("Next")', timeout=5)
+        print("successfully access google ads")
+        time.sleep(20)
+        self.sb.click('//*[@id="navigation.tools"]/div/a/rail-item', timeout=15, delay=1)
+        self.sb.click(
+            "/html/body/div[1]/root/div/div[1]/div[2]/div/div[3]/div/div/awsm-child-content/div[1]/div[2]/kp-root/div/div/"
+            "view-loader[2]/splash-view/div/div/div[1]/splash-cards/div/div[1]",
+            timeout=15,
+            delay=1,
+        )
+        print("click success 2")
--- googleAds.py
+++ googleAds.py
@@ -6,36 +6,39 @@ import re
 import html
 import requests
 
-from email_google import access_google_ads
-from keyworkCPC import keywordCPC
+from email_google import EmailGoogleAccess
+from keyworkCPC import KeywordCPC
 from BeeModule import BeeHelper
 
-def clean_html_tags(text):
-    cleaned_text = re.sub(r"<.*?>", "", text)
-    cleaned_text = cleaned_text.replace("\n", "").strip()
-    return cleaned_text
+class MainClass:
+    def __init__(self):
+        self.sb_config = {
+            "uc": True,
+            "device_metrics": None,
+            "is_mobile": None,
+            "agent": None,
+        }
+        self.sb = None
+        self.email_google = None
 
-if __name__ == "__main__":
+    def clean_html_tags(self, text):
+        cleaned_text = re.sub(r"<.*?>", "", text)
+        cleaned_text = cleaned_text.replace("\n", "").strip()
+        return cleaned_text
+
+    def run(self):
         try:
-            sb_config = {
-                "uc": True,
-                "device_metrics": None,
-                "is_mobile": None,
-                "agent": None,
-            }
             with SB(
-                uc=sb_config["uc"],
+                uc=self.sb_config["uc"],
                 maximize=True,
-                device_metrics=sb_config["device_metrics"],
-                is_mobile=sb_config["is_mobile"],
-                agent=sb_config["agent"],
-            ) as sb:
-                
-
-                email_google = access_google_ads(sb)
+                device_metrics=self.sb_config["device_metrics"],
+                is_mobile=self.sb_config["is_mobile"],
+                agent=self.sb_config["agent"],
+            ) as self.sb:
+                self.email_google = EmailGoogleAccess(self.sb).access_google_ads()
                 final = []
                 while True:
-                    final = keywordCPC(sb, email_google, final)
+                    final = KeywordCPC(self.sb, self.email_google).keyword_cpc()
 
                     helper = BeeHelper()  
                     url = 'https://dev92.beetech.one/test_post'
@@ -46,3 +49,7 @@ if __name__ == "__main__":
 
         except Exception as e:
             print(str(e))
+
+if __name__ == "__main__":
+    main_instance = MainClass()
+    main_instance.run()
